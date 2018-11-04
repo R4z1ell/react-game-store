4242aaf17357786828178e4bcbd9b040a751950c
R4z1ell <creawshaka@gmail.com>
2018-11-04 23:47:48 +0100

    working on carousel

--- client/src/components/Home/home_carousel/home_carousel.js
+++ client/src/components/Home/home_carousel/home_carousel.js
@@ -2,17 +2,69 @@ import React from 'react';
 import Slider from 'react-slick';
 
 import style from './home_carousel.module.scss';
+import './slider.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'transparent' }}
+      onClick={onClick}
+    >
+      <svg
+        viewBox="0 0 15 24"
+        width="100%"
+        height="100%"
+        fill="#fff"
+        className="prev-arrow"
+      >
+        <path d="M0 21.16L9.16 12 0 2.82 2.82 0l12 12-12 12z" />
+      </svg>
+    </div>
+  );
+}
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'transparent' }}
+      onClick={onClick}
+    >
+      <svg viewBox="0 0 15 24" width="100%" height="100%" fill="#fff">
+        <path d="M0 21.16L9.16 12 0 2.82 2.82 0l12 12-12 12z" />
+      </svg>
+    </div>
+  );
+}
 
 const HomeCarousel = props => {
   const settings = {
     dots: true,
+    dotsClass: 'slick-dots',
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
-    autoplay: false,
-    autoplaySpeed: 3000
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    customPaging: i => (
+      <div
+        style={{
+          width: '41px',
+          height: '2px',
+          padding: '1px',
+          backgroundColor: 'blue'
+        }}
+      />
+    )
   };
 
   const generateGameSlides = () =>
@@ -21,7 +73,9 @@ const HomeCarousel = props => {
           <div key={i}>
             <div
               style={{
-                background: `url(${game.images.background})`,
+                background: `linear-gradient(180deg,transparent 0,rgba(0,0,0,.6)),url(${
+                  game.images.background
+                })`,
                 height: '490px'
               }}
             >
@@ -30,6 +84,11 @@ const HomeCarousel = props => {
                   {game.images.logo ? (
                     <img src={`${game.images.logo}`} alt="logo" />
                   ) : null}
+                </div>
+                <div className={style.game__above}>
+                  <img src="/images/windows.png" alt="window-logo" />
+                  <FontAwesomeIcon icon={faCircle} />
+                  <span>Now available</span>
                 </div>
                 <div className={style.game__title}>{game.title}</div>
               </div>
