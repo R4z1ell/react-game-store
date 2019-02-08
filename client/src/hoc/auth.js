1e92c594a8b1b21054ab39858724ef703d96cc91
R4z1ell <creawshaka@gmail.com>
2019-02-09 00:05:13 +0100

    added 'Loader' spinner Component that show before data from server is up

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Loader from 'react-loader-spinner';
+
 import { auth } from '../store/actions/user_actions';
-//import CircularProgress from '@material-ui/core/CircularProgress';
 
 export default function(ComposedClass, adminRoute = null) {
   class AuthenticationCheck extends Component {
-    // state = {
-    //   loading: true
-    // };
+    state = {
+      loading: true
+    };
 
     componentDidMount() {
-      this.props.dispatch(auth());
+      this.props.dispatch(auth()).then(response => {
+        let user = this.props.user.userData;
+
+        if (user.cart) {
+          this.setState({ loading: false });
+        }
+      });
     }
 
     render() {
-      //   if (this.state.loading) {
-      //     return (
-      //       <div className="main_loader">
-      //         <CircularProgress style={{ color: '#2196f3' }} thickness={7} />
-      //       </div>
-      //     );
-      //   }
-      return <ComposedClass {...this.props} user={this.props.user} />;
+      const { loading } = this.state;
+      return !loading ? (
+        <ComposedClass {...this.props} user={this.props.user} />
+      ) : (
+        <div
+          style={{
+            margin: '0 auto',
+            height: '100vh',
+            width: '1100px',
+            textAlign: 'center',
+            marginTop: '250px'
+          }}
+        >
+          <Loader type="RevolvingDot" color="#404040" height="80" width="80" />
+        </div>
+      );
     }
   }
 
