f15851852f813775500a35e81529fc336e0dc914
R4z1ell <creawshaka@gmail.com>
2019-04-17 20:02:11 +0200

    added 'heroku-postbuild' script

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +16,14 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 require('./routes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
